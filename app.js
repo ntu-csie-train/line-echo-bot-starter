a185abd7997faeb3a45e2c7e3ed0bf6f427473eb
ggm <godgunman@gmail.com>
2017-07-22 15:06:58 +0800

    project init

---
+++ app.js
@@ -0,0 +1,48 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+let request = require('request')
+let app = express()
+
+const CHANNEL_ACCESS_TOKEN = 'Your_Channel_Access_Token'
+const PORT = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.listen(PORT, function () {
+    console.log(`App listening on port ${PORT}!`)
+})
+
+// handler receiving messages
+app.post('/', function (req, res) {
+})
+
+// generic function sending messages
+function sendMessage(replyToken, text) {
+    let body = {
+        replyToken,
+        messages: [{
+            type: 'text',
+            text,
+        }],
+    };
+
+    let options = {
+        url: 'https://api.line.me/v2/bot/message/reply',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${CHANNEL_ACCESS_TOKEN}`,
+        },
+        body,
+        json: true,
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        }
+        else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    })
+}
